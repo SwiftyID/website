6d60304cb0a48cf23521d0f474aae0f3c4bf1ad7
Seto Elkahfi <setoelkahfi@gmail.com>
2018-06-27 12:47:58 +0700

    Upgrade to vapor 3.0

--- Package.swift
+++ Package.swift
@@ -1,21 +1,25 @@
 // swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "website",
+    name: "swiftyid-website",
+    products: [
+        .library(name: "App", targets: ["App"]),
+        .executable(name: "Run", targets: ["Run"])
+    ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "2.4.2"),
-        .package(url: "https://github.com/vapor/leaf-provider.git", from: "1.0.0")
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.2.0")),
+        .package(url: "https://github.com/vapor/leaf-provider.git", .upToNextMajor(from: "1.1.0")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "website",
-            dependencies: ["Vapor", "LeafProvider"]),
+            name: "App",
+            dependencies: ["Vapor", "LeafProvider"],
+            exclude: ["Config", "Database", "Public", "Resources"]
+        ),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
     ]
 )
+
